0441dc0095fd5bfa00c5a942accb165a19e0638f
MUGALULA WARREN <omnikkotow@gmail.com>
2022-06-24 15:32:45 +0300

    editing urls

--- library_site/bookapp/views.py
+++ library_site/bookapp/views.py
@@ -3,5 +3,35 @@ from django.shortcuts import render
 # Create your views here.
 
 #shared views
-def login_form(request):
-    return render(request, "")
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.get('username')
+        username = request.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+            
+
+
+
+    context ={}
+    return render(request, 'base/login_register.html', context)
+
+
+def home(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q)|
+        Q(description__icontains=q)
+    )
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+
+    context = {'rooms':rooms, 'topics':topics, 'room-count':room_count}
+    return render(request, 'base/home.html', context)
+
