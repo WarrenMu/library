d7b80102bb3f6a6f04a62e6afe5490d39a9e74ee
MUGALULA WARREN <omnikkotow@gmail.com>
2022-06-24 15:38:22 +0300

    editing urls

--- library_site/bookapp/urls.py
+++ library_site/bookapp/urls.py
@@ -1,7 +1,12 @@
 
 from django.urls import URLPattern, re_path,path
 from . import views
-urlpatterns = [
 
-    path('bookapp/', views.loginPage, name="login"),
-]
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('room/<str:pk>/', views.room, name="room"),
+    path('create-room/', views.createRoom, name="create-room"),
+    path('update-room/<str:pk>/', views.updatedRoom, name="update-room"),
+    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
+    path('login/', views.loginPage, name="login"),
+] 
