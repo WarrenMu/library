7a20574dc278811ba1e7c190b37c6f1cef9dab18
MUGALULA WARREN <omnikkotow@gmail.com>
2022-06-24 12:11:00 +0300

    editing installed apps

--- library_site/bookapp/urls.py
+++ library_site/bookapp/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import RedirectView
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bookapp/', include('bookapp.urls')),
-    path('', RedirectView.as_view(url='bookapp/', permanent=True)),
 ]
