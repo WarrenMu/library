c16a54975dfc2cc886844c6e5292e46a3b15cc21
MUGALULA WARREN <omnikkotow@gmail.com>
2022-06-28 11:34:24 +0300

    migrating

--- core/library/views.py
+++ core/library/views.py
@@ -142,3 +142,83 @@ def return_book(request,issueID):
     issue.returned=True
     issue.save()
     return redirect('/all-issues/')
+
+#  FINES
+
+@login_required(login_url='/student/login/')
+@user_passes_test(lambda u: not u.is_superuser ,login_url='/student/login/')
+def myfines(request):
+    if Student.objects.filter(student_id=request.user):
+        student=Student.objects.filter(student_id=request.user)[0]
+        issues=Issue.objects.filter(student=student)
+        for issue in issues:
+            calcFine(issue)
+        fines=Fine.objects.filter(student=student)
+        return render(request,'library/myfines.html',{'fines':fines})
+    messages.error(request,'You are Not a Student !')
+    return redirect('/')
+
+@login_required(login_url='/student/login/')
+@user_passes_test(lambda u:  u.is_superuser ,login_url='/admin/')
+def allfines(request):
+    issues=Issue.objects.all()
+    for issue in issues:
+        calcFine(issue)
+    return redirect('/admin/library/fine/')
+
+@login_required(login_url='/student/login/')
+@user_passes_test(lambda u:  u.is_superuser ,login_url='/admin/')
+def deletefine(request,fineID):
+    fine=Fine.objects.get(id=fineID)
+    fine.delete()
+    return redirect('/all-fines/')
+
+import razorpay
+razorpay_client = razorpay.Client(auth=(settings.RAZORPAY_KEY_ID, settings.RAZORPAY_KEY_SECRET))
+
+@login_required(login_url='/student/login/')
+@user_passes_test(lambda u: not u.is_superuser ,login_url='/student/login/')
+def payfine(request,fineID):
+    fine=Fine.objects.get(id=fineID)
+    order_amount = int(fine.amount)*100
+    order_currency = 'INR'
+    order_receipt = fine.order_id
+    
+    
+    razorpay_order=razorpay_client.order.create(dict(amount=order_amount, currency=order_currency, receipt=order_receipt, ))
+    print(razorpay_order)
+    
+    
+    return render(request,'library/payfine.html',
+    {'amount':order_amount,'razor_id':settings.RAZORPAY_KEY_ID,
+    'reciept':razorpay_order['id'],
+    'amount_displayed':order_amount / 100,
+    'address':'a custom address',
+    'fine':fine, 
+    })
+
+
+@login_required(login_url='/student/login/')
+@user_passes_test(lambda u: not u.is_superuser ,login_url='/student/login/')
+def pay_status(request,fineID):
+    if request.method == 'POST':
+        params_dict={
+            'razorpay_payment_id':request.POST['razorpay_payment_id'],
+            'razorpay_order_id':request.POST['razorpay_order_id'],
+            'razorpay_signature':request.POST['razorpay_signature'],
+        }
+        try:
+            status=razorpay_client.utility.verify_payment_signature(params_dict)
+            if status is None:
+                fine=Fine.objects.get(id=fineID)
+                fine.paid=True
+                fine.datetime_of_payment=timezone.now()
+                fine.razorpay_payment_id=request.POST['razorpay_payment_id']
+                fine.razorpay_signature=request.POST['razorpay_signature']
+                fine.razorpay_order_id = request.POST['razorpay_order_id']
+                fine.save()
+                
+            messages.success(request,'Payment Succesfull')
+        except:
+            messages.error(request,'Payment Failure')
+    return redirect('/my-fines/')
