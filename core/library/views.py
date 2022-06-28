15c3c0b23444b940425099b8004e3d7d179a6f53
MUGALULA WARREN <omnikkotow@gmail.com>
2022-06-28 11:10:36 +0300

    editing views

--- core/library/views.py
+++ core/library/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import Book, Author, Isssue, Fine
+from student.models import Student
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib import messages
