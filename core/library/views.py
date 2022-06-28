d631bc2bab1d2b05f2fef9a42e564da0a80146fe
MUGALULA WARREN <omnikkotow@gmail.com>
2022-06-28 11:25:53 +0300

    editing views

--- core/library/views.py
+++ core/library/views.py
@@ -68,3 +68,36 @@ def deletebook(request,bookID):
     messages.success(request,'Book - {} Deleted succesfully '.format(book.name))
     book.delete()
     return redirect('/')
+
+#  ISSUES
+
+@login_required(login_url='/student/login/')
+@user_passes_test(lambda u: not u.is_superuser,login_url='/student/login/')
+def issuerequest(request,bookID):
+    student=Student.objects.filter(student_id=request.user)
+    if student:
+        book=Book.objects.get(id=bookID)
+        issue,created=Issue.objects.get_or_create(book=book,student=student[0])
+        messages.success(request,'Book - {} Requested succesfully '.format(book.name))
+        return redirect('home')
+    
+    messages.error(request,'You are Not a Student !')
+    return redirect('/')
+
+@login_required(login_url='/student/login/')
+@user_passes_test(lambda u: not u.is_superuser ,login_url='/student/login/')
+def myissues(request):
+    if Student.objects.filter(student_id=request.user):
+        student=Student.objects.filter(student_id=request.user)[0]
+        
+        if request.GET.get('issued') is not None:
+            issues=Issue.objects.filter(student=student,issued=True)
+        elif request.GET.get('notissued') is not None:
+            issues=Issue.objects.filter(student=student,issued=False)
+        else:
+            issues=Issue.objects.filter(student=student)
+
+        return render(request,'library/myissues.html',{'issues':issues})
+    
+    messages.error(request,'You are Not a Student !')
+    return redirect('/')
