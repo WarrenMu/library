c587f46bd771d97a5bc8bcf4b8208ca3a5b3939c
MacUser <luwagamicheal1@gmail.com>
2022-07-20 13:31:17 +0300

    Updated urls file

--- library/urls.py
+++ library/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path
 from .views import allbooks,search,addbook,deletebook,issuerequest,myissues,issue_book,return_book,requestedissues,sort
+urlpatterns = [
+    path('',allbooks,name='home'),
+    path('search/',search),
+
+]
