66e4f7fe2e4eed1251a825d32ea8c95aa8b2fa79
MUGALULA WARREN <omnikkotow@gmail.com>
2022-07-20 11:00:58 +0300

    editing

--- library/urls.py
+++ library/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from accounts.views import registerView
 from .views import allbooks,search,addbook,deletebook,issuerequest,myissues,issue_book,return_book,requestedissues,sort
-#,myfines,allfines,deletefine,payfine,pay_status,
+
 
 urlpatterns = [
     path('',allbooks,name='home'),
@@ -13,8 +13,8 @@ urlpatterns = [
     path('request-book-issue/<int:bookID>/',issuerequest),
     path('my-issues/',myissues),
     path('all-issues/',requestedissues),
-    # path('all-fines/',allfines),
+ 
     path('issuebook/<int:issueID>/',issue_book),
     path('returnbook/<int:issueID>/',return_book),
-    # path('delete-fine/<int:fineID>/',deletefine),
+  
 ]
